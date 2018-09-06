e908d6c037f9580244f344ddd1cc70add5c6d1f7
long-yong <long.yong@outlook.com>
2018-09-05 17:15:41 -0700

    Add classes

---
+++ src/main/cls/practice/HashMapFun.java
@@ -0,0 +1,23 @@
+package cls.practice;
+import java.util.HashMap;
+
+public class HashMapFun {
+
+    public static void main(String[] args) {
+        HashMapFun obj = new HashMapFun();
+        HashMap<String,String> map = createMap();
+        showMap(map);
+    }
+
+    public static HashMap<String,String> createMap() {
+        HashMap<String,String> map = new HashMap<String,String>();
+        for(int i=0; i<10; i++) map.put("key"+i, "val"+i);
+        return map;
+    }
+
+    public static void showMap(HashMap<String,String> map){
+        for(Object key:map.keySet()) {
+            System.out.println(key + " : " + map.get(key));
+        }
+    }
+}
