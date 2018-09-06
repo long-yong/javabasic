903d674f131ac6cc72625f9f2de389abb400a8ee
long-yong <long.yong@outlook.com>
2018-09-06 10:24:26 -0700

    chg1

--- src/main/node/ListTester.java
+++ src/main/node/ListTester.java
@@ -2,6 +2,7 @@ package node;
 
 public class ListTester {
     public static void main(String[] args) {
+
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.add(3);
         sll.add(4);
