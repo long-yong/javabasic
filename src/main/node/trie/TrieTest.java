a5b67ba9cbd5834cb61db170e659e646885f8164
long-yong <long.yong@outlook.com>
2018-09-07 08:59:05 -0700

    chg6 trie

---
+++ src/main/node/trie/TrieTest.java
@@ -0,0 +1,16 @@
+package node.trie;
+
+public class TrieTest {
+
+        public static void main(String[] args) {
+
+                Trie trie = new Trie();
+                trie.insertWord("car");
+                trie.insertWord("card");
+                trie.insertWord("chip");
+                trie.insertWord("trie");
+                trie.insertWord("try");
+
+        }
+
+}
